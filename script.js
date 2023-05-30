2fc5818d97273055fff3d966af06f1c4a6645175
Youssuf-bakry <youssuf.bakry@gmail.com>
2023-05-30 06:00:19 +0300

    javascript close btn

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
 const btnOpen = document.getElementById("open");
+const btnClose = document.getElementById("close");
+
 const circle = document.querySelector(".circle");
 const container = document.querySelector(".container");
+
 btnOpen.addEventListener("click", (e) => {
   circle.classList.add("rotate");
   container.classList.add("rotate");
+});
+btnClose.addEventListener("click", (e) => {
+  circle.classList.remove("rotate");
+  container.classList.remove("rotate");
 });
